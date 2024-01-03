91667778f612ae19bf43d7308d366cef97a2aefc
Jacob Jung <jacobj@live.ca>
2024-01-02 21:58:27 -0500

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import Image from 'next/image'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Home() {
   return (
-    <div>
-      
+    
+    <div style={{ marginLeft: '20px', marginRight: '20px', marginTop: '20px' }}>
+      <Card>
+  <CardHeader>
+    <CardTitle>Sign in</CardTitle>
+    <CardDescription>to continue to application</CardDescription>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
+
     </div>
   )
 }
